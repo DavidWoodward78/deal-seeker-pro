fb30200bb056bf72b7f4d061253c75607e74d4a8
psychotron78 <88747161+psychotron78@users.noreply.github.com>
2024-12-31 17:42:32 -0500

    Add Product type and fix TypeScript error

---
+++ pages/blog/top-air-fryers.tsx
@@ -0,0 +1,9 @@
+export default function TopAirFryers() {
+    return (
+      <div>
+        <h1>Top Air Fryers</h1>
+        <p>Check out our top picks for air fryers!</p>
+      </div>
+    );
+  }
+  
---
+++ pages/contact.tsx
@@ -0,0 +1,9 @@
+export default function Contact() {
+    return (
+      <div>
+        <h1>Contact Us</h1>
+        <p>Please reach out to us for more information.</p>
+      </div>
+    );
+  }
+  
